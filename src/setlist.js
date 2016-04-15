979b7887d65a8803e7ad1238421888df4426fd01
Eric Castoldi <ericgtr17@gmail.com>
2016-04-15 19:38:53 -0300

    Melhorias visuais - Inclusão do skeleton-css

--- src/setlist.js
+++ src/setlist.js
@@ -1,19 +1,29 @@
 var  Setlist = React.createClass({
   render : function(){
+
     var songs = this.props.data.map(function (song) {
       
       return (
-        <li>
-          <Song artist={song.artist} song={song.song} tags={song.tags.join(', ')} />
-        </li>
+        <tr>
+          <td>
+            <Song artist={song.artist} song={song.song} tags={song.tags.join(', ')} />
+          </td>
+        </tr>
       );
 
     });
 
     return (
-        <ul className="setlist">
+      <table className="u-full-width">
+        <thead>
+          <tr>
+            <th>Músicas</th>
+          </tr>
+        </thead>
+        <tbody>
           { songs }
-        </ul>
+        </tbody>
+      </table>
       );
   }
 });
