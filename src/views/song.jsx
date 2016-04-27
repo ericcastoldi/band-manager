e48640db9c5ee0243b1de9833340d9195e9446dd
Eric.Castoldi <eric.castoldi@senior.com.br>
2016-04-27 10:04:28 -0300

    Melhoria nos scripts gulp: uso de browserify e script completo de clean, install dependencies e launch da aplicação

--- src/views/song.jsx
+++ src/views/song.jsx
@@ -1,11 +1,14 @@
+var React = require('react');
+
 var Song = React.createClass({
   render: function() {
     return (
       <div className="song">
         <em>{this.props.artist} - {this.props.song}</em>
-        <br />
         <small>{this.props.tags}</small>
       </div>
     );
   }
 });
+
+module.exports = Song;
