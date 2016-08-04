85353fa0adee5100c331528e7c1bbae79b4cf33a
Eric Castoldi <ericgtr17@gmail.com>
2016-08-03 22:12:02 -0300

    Https force ssl
    http://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect/23894573#23894573

--- app.js
+++ app.js
@@ -6,19 +6,41 @@ var setlist = new Setlist();
 
 var app = express();
 
+var env = process.env.NODE_ENV || 'development';
+
+var forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+app.configure(function () {
+
+  if (env === 'production') {
+    app.use(forceSsl);
+  }
+
+  // other configurations etc for express go here...
+});
+
+
+
 app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Cache-Control', 'no-cache');
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-cache');
 
-    next();
+  next();
 });
 
 app.get('/api/setlist', setlist.all);
@@ -26,6 +48,6 @@ app.post('/api/setlist', setlist.update);
 app.post('/api/filterSetlist', setlist.byTags);
 
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('The dark magic is happening at http://localhost:' + app.get('port') + '/');
 });
